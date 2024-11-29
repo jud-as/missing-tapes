02339103c913a6d1b081126916499fd9ef936c85
jud-as <guilhermeferreira082@gmail.com>
2024-11-29 18:35:13 -0300

    Resolvendo conflitos de fetch

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,33 @@
-import express from "express"
+import express from 'express';
 
-const app = express()
 
-const users = []
+import { registerUser, loginUser, logoutUser, editUser, removeUser } from '../model/user';
 
-app.post('/usuario', (request, response) => {
-    console.log(Request)
-    response.send('Ok POST')
-})
+const router = express.Router();
+
+router.post('/usuario/registro', (req, res) => {
+    const result = registerUser(req.body);
+    res.json(result);
+});
+
+router.post('/usuario/login', (req, res) => {
+    const result = loginUser(req.body);
+    res.json(result);
+});
+
+router.post('/usuario/logout', (req, res) => {
+    const result = logoutUser(req.body);
+    res.json(result);
+});
+
+router.post('/usuario/editar', (req, res) => {
+    const result = editUser(req.body);
+    res.json(result);
+});
+
+router.post('/usuario/remover', (req, res) => {
+    const result = removeUser(req.body);
+    res.json(result);
+});
+
+export default router;
